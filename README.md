f5643e80202b309e4461e2729febe905c4d95a91
edosyhptra <edo23syahputra@gmail.com>
2024-05-27 15:54:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# session11
+
+This code adapted from Adam Woodbeck's textbook entitled Network and System Programming in Go.
---
+++ protobuf/housework.go
@@ -0,0 +1,31 @@
+package protobuf
+
+import (
+	"io"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/edosyhptra/session11/housework/v1"
+)
+
+func Load(r io.Reader) ([]*housework.Chore, error) {
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var chores housework.Chores
+
+	return chores.Chores, proto.Unmarshal(b, &chores)
+}
+
+func Flush(w io.Writer, chores []*housework.Chore) error {
+	b, err := proto.Marshal(&housework.Chores{Chores: chores})
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(b)
+
+	return err
+}
